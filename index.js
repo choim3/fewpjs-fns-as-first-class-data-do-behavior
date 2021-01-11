038fe9249bc63901cc2bde1fa9fcfe4a1da84775
choim3 <choim3@spu.edu>
2021-01-11 11:26:30 -0800

    commit tree aa18b8473654060311f29dc05137065f5f16040f

--- index.js
+++ index.js
@@ -11,3 +11,22 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(timeString) {
+  const splitTime = timeString.split(':');
+  const convertTime = parseInt(splitTime);
+
+  if (convertTime < 12) {
+    return "Good Morning"
+  }
+  else if (convertTime > 12 && convertTime < 17) {
+    return "Good Afternoon"
+  }
+  else {
+    return "Good Evening"
+  }
+}
+
+function displayMessage(message) {
+  document.getElementById('greeting').innerText = message;
+}
